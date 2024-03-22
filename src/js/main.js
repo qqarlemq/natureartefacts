1d5f6053c3f07265346a20f06b5fd37e3f10c3cd
qqarlem <qqarlemq@gmail.com>
2024-03-22 21:16:58 +0300

    third commit

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
+    },
+  });
